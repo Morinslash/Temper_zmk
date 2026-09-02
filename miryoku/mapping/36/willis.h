ffe0626dafc1be19558f7406406b809f26397061
Sebastian Kulinski <41920857+Morinslash@users.noreply.github.com>
2026-09-02 22:17:21 +0200

    add whole miryoku

--- miryoku/mapping/36/willis.h
+++ miryoku/mapping/36/willis.h
@@ -1,9 +1,29 @@
-// Copyright 2022 Manna Harbour
+// Copyright 2024 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#if !defined (MIRYOKU_LAYOUTMAPPING_MINIDOX)
+#if !defined (MIRYOKU_LAYOUTMAPPING_WILLIS)
 
-#define MIRYOKU_LAYOUTMAPPING_MINIDOX( \
+#define XXX &none
+
+#if defined (MIRYOKU_MAPPING_2X2U)
+#define MIRYOKU_LAYOUTMAPPING_WILLIS( \
+K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+K00  K01  K02  K03  K04       K05  K06  K07  K08  K09 \
+K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
+K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 \
+          K34       K33       K36       K35
+
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 30 31
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 32 33
+
+#define MIRYOKU_KLUDGE_THUMBCOMBOS
+
+#else
+#define MIRYOKU_LAYOUTMAPPING_WILLIS( \
 K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
 K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
 K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
@@ -27,7 +47,19 @@ K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 \
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 31 32
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 33 34
 
+#endif
+
+/ {
+  chosen {
+      zmk,physical-layout =
+#if defined MIRYOKU_MAPPING_2X2U
+      &willis_bruce_layout;
+#else
+      &willis_minidox_layout;
+#endif
+  };
+};
 
 #endif
 
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_MINIDOX
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_WILLIS
